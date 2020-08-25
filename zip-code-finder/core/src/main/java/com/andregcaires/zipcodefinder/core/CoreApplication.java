8ce1c7396313487c937b67d40407e62cb270a803
= <=>
2020-08-25 18:01:39 -0300

    project created

---
+++ zip-code-finder/core/src/main/java/com/andregcaires/zipcodefinder/core/CoreApplication.java
@@ -0,0 +1,9 @@
+package com.andregcaires.zipcodefinder.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+
+}
---
+++ zip-code-finder/core/src/main/java/com/andregcaires/zipcodefinder/core/services/ZipCodeFinderServiceImpl.java
@@ -0,0 +1,5 @@
+package com.andregcaires.zipcodefinder.core.services;
+
+public class ZipCodeFinderServiceImpl {
+
+}
